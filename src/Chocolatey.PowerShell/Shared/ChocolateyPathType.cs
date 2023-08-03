fcd0371125c2cea7ea5d41a0be0be8e02aa07b58
Rain Sallow <rain@chocolatey.io>
2023-08-03 13:55:09 -0400

    (#3051) Rewrite Get-ChocolateyPath helper command
    Rewrite of the Get-ChocolateyPath into C# cmdlet, and associated
    changes to helper methods.

---
+++ src/Chocolatey.PowerShell/Shared/ChocolateyPathType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chocolatey.PowerShell.Shared
+{
+    public enum ChocolateyPathType
+    {
+        PackagePath,
+        InstallPath,
+    }
+}
